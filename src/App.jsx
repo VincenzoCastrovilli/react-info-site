e6b3ac089b1d5165cf978876a3e3022f10be873b
VincenzoCastrovilli <vcastrovilli@icloud.com>
2024-01-22 16:53:20 +0100

    Add project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import './App.css';
+
 export default function App() {
   return (
-    <div>
-      <img
-        src="src\assets\icons\React-icon.png"
-        alt="React logo"
-        width="40px"
-      />
-
-      <h1>Fun fact about React</h1>
-
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is mantained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
+    <>
+      <Navbar />
+      <Main />
+    </>
   );
 }
