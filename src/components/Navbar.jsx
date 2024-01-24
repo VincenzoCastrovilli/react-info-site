4efcf92ef2ab9eaeefe95b3c621646c5794fc090
VincenzoCastrovilli <vcastrovilli@icloud.com>
2024-01-24 18:00:07 +0100

    Fix images for Netlify 2.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,10 @@
+import reactIcon from '../assets/icons/React-icon.png';
+
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <img
-          src="./src/assets/icons/React-icon.png"
-          alt="React logo"
-          className="image"
-        />
+        <img src={reactIcon} alt="React logo" className="image" />
         <p className="name">ReactFacts</p>
       </div>
       <div className="info">React Course - Project 1</div>
